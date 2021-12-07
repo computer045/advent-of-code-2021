6be64ad593c819f8b4e738e998779492ff14f134
Louis-Philippe Fortin <louis-philippe.fortin@canada.ca>
2021-12-07 10:13:57 -0500

    day7 code

---
+++ day7/main.rb
@@ -0,0 +1,31 @@
+def part1(crabs)
+  crabs.sort!
+  i = (crabs.count / 2) - 1
+  median = crabs.count.even? ? (crabs[i.floor] + crabs[i.ceil]) / 2 : crabs[i]
+  fuel_sum = 0
+  crabs.each do |c|
+    fuel_sum += (c - median).abs
+  end
+  return fuel_sum
+end
+
+def part2(crabs)
+  fuel_sums = []
+  for i in 0..crabs.max do
+    sum = 0
+    crabs.each do |c|
+      sum += (1..(c - i).abs).sum
+    end
+    fuel_sums.push(sum)
+  end
+  return fuel_sums.min
+end
+
+def main
+  input = File.readlines('./input.txt').map(&:chomp)[0].split(',').map(&:to_i)
+
+  puts part1(input)
+  puts part2(input)
+end
+
+main
