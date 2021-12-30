2f6afe966209eeec7f35f2012d39535bafdde569
Louis-Philippe Fortin <timemaster.lpf@gmail.com>
2021-12-30 17:15:06 -0500

    day11 part1 completed

--- day11/main.rb
+++ day11/main.rb
@@ -1,22 +1,34 @@
 class Octopus
-  attr_reader :step, :flashes
-  def initialize(step)
-    @step = step
+  attr_reader :energy, :flashes
+  def initialize(energy)
+    @energy = energy
     @flashes = 0
+    @can_flash = true
   end
   def to_s
-    return @step
+    return @energy
   end
   def energize
-      @step += 1
+    if @can_flash then
+      @energy += 1
+    end
   end
   def flash
     @flashes += 1
-    @step = 0
+    @energy = 0
+    @can_flash = false
+  end
+  def reset
+    @can_flash = true
   end
 end
 
 def step(octopuses)
+  octopuses.each do |line|
+    line.each do |column|
+      column.reset()
+    end
+  end
   octopuses.each do |line|
     line.each do |column|
       column.energize()
@@ -26,9 +38,10 @@ def step(octopuses)
 end
 
 def propagate(octopuses)
+  step_indexes = []
   octopuses.each_index do |line|
     octopuses[line].each_index do |column|
-      if (octopuses[line][column].step > 9) then
+      if (octopuses[line][column].energy > 9) then
         octopuses = energize_around(octopuses, column, line)
       end
     end
@@ -41,7 +54,7 @@ def energize_around(octopuses, pos_x, pos_y)
     not octopuses[pos_y][pos_x].nil? and
     pos_y >= 0 and pos_x >= 0 and
     pos_x < octopuses[0].count and pos_y < octopuses.count then
-    if octopuses[pos_y][pos_x].step > 9 then
+    if octopuses[pos_y][pos_x].energy > 9 then
       octopuses[pos_y][pos_x].flash
       energize_around(octopuses, (pos_x + 1), pos_y)
       energize_around(octopuses, (pos_x - 1), pos_y)
@@ -53,9 +66,12 @@ def energize_around(octopuses, pos_x, pos_y)
       energize_around(octopuses, (pos_x - 1), (pos_y - 1))
     else
       octopuses[pos_y][pos_x].energize
+      if octopuses[pos_y][pos_x].energy > 9 then
+        energize_around(octopuses, pos_x, pos_y)
+      end
     end
   end
-  
+
   return octopuses
 end
 
@@ -66,12 +82,12 @@ def part1(octopuses, nb_steps)
   count = 0
   octopuses.each do |line|
     line.each do |column|
-      count += column.flashes 
+      count += column.flashes
     end
   end
 
-  #return count
-  return octopuses
+  return count
+  #return octopuses.map{|l| l.map(&:to_s) }
 end
 
 def part2(input)
@@ -93,8 +109,8 @@ def main
     5283751526
   TEXT
   small_input = small_input.split("\n").map(&:chomp).map {|l| l.chars }.map{ |s| s.map { |c| Octopus.new(c.to_i) } }
-  #puts part1(input)
-  pp part1(small_input, 2)
+  puts part1(input, 100)
+  #pp part1(small_input, 100)
   #puts part2(input)
 end
 
