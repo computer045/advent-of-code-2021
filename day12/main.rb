ec2f40646520003144f9620b5ef28b69daab9d44
Louis-Philippe Fortin <timemaster.lpf@gmail.com>
2021-12-31 01:12:39 -0500

    day12 init

---
+++ day12/main.rb
@@ -0,0 +1,15 @@
+def part1(input)
+  return 0
+end
+
+def part2(input)
+  return 0
+end
+
+def main
+  input = File.readlines('./input.txt').map(&:chomp)
+  puts part1(input)
+  puts part2(input)
+end
+
+main
